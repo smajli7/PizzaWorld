3ce828048a098ddb3887f41c011f25c396c84819
luigids03 <luigi.da.silva@hotmail.com>
2025-05-11 12:58:23 +0200

    PizzaWorld Initialisierung

---
+++ PizzaWorld+/src/main/java/pizzaworld/PizzaWorldApplication.java
@@ -0,0 +1,13 @@
+package pizzaworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaWorldApplication.class, args);
+	}
+
+}
---
+++ PizzaWorld+/src/test/java/pizzaworld/PizzaWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package pizzaworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

50e1884df25dfb86c2cd291260f55956b0be0b4f
louisa <louisabahr43@gmail.com>
2025-06-12 12:33:20 +0200

    dashboard I

--- PizzaWorld+/frontend/src/app/pages/dashboard/dashboard.component.ts
+++ PizzaWorld+/frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']   //  ✅ plural + Array
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}

edd9edcb1f22d2b70e6726561cdb3b36cab802b8
smajli7 <jakub.naruszko@stud.fra-uas.de>
2025-06-24 13:06:13 +0200

    fix?

--- PizzaWorld+/frontend/src/app/app.component.ts
+++ PizzaWorld+/frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';  
 import { AuthService } from './core/auth.service'; // Import AuthService
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+    templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterOutlet] // Import RouterOutlet for routing
 })
 export class AppComponent {
   constructor(private auth: AuthService) {

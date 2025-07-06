bdbe58bed62a953bc85df8fcfed8a42b03d2e555
smajli7 <jakub.naruszko@stud.fra-uas.de>
2025-07-06 17:39:57 +0200

    api keysxd

--- PizzaWorld/start.sh
+++ PizzaWorld/start.sh
@@ -13,7 +13,7 @@ export DB_PASSWORD="PizzaWorld.2025"
 export JWT_SECRET="supergeheimerSchluessel123456789012345"
 export GMAIL_APP_PASSWORD="obcs zapk yyqb wedb"
 # Google AI Configuration for Local Testing
-export GOOGLE_AI_API_KEY="x"
+export GOOGLE_AI_API_KEY="AIzaSyChBAaKN26OKY3itD03JiAEIq7J5GCWg-U"
 export GOOGLE_AI_MODEL="gemma-3n-e2b-it"
 
 echo ""
